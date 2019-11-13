e8625922e4838bbbc91d3d9cd90554d75c462117
jenkins <jenkins.aspose@gmail.com>
2019-11-13 16:34:07 +0400

    SDK version 19.11.40 generated

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "AsposeEmailCloudSdk"
-VERSION = "1.0.0"
+VERSION = "19.11.40"
 # To install the library, run the following
 #
 # python setup.py install
