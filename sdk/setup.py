cd09f1bc68cf181d97baed5ace561ae2683dc192
Marat Gumerov <marat.gumerof@aspose.com>
2019-12-19 18:16:37 +0400

    Package description improved (license, home page)

--- sdk/setup.py
+++ sdk/setup.py
@@ -30,13 +30,17 @@ with open(path.join(this_directory, "..", 'README.md'), encoding='utf-8') as f:
 setup(
     name=NAME,
     version=VERSION,
+    url='https://products.aspose.cloud/email/family',
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+    ],
+    license = "License :: OSI Approved :: MIT License",
     description="Aspose.Email Cloud API",
     author_email="",
-    url="",
     keywords=["Swagger", "Aspose.Email Cloud API"],
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
 )
