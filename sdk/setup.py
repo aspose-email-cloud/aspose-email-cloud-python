907d204223bc4d8a54efc3aaac2b667b4b6038eb
Aspose.Email Team <aspose.email.team@gmail.com>
2020-05-27 01:29:36 -0500

    SDK version 20.5.0.86 generated

--- sdk/setup.py
+++ sdk/setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "aspose-email-cloud"
-VERSION = "20.5.0.79"
+VERSION = "20.5.0.86"
 # To install the library, run the following
 #
 # python setup.py install
